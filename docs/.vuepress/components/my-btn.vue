866dba63570a3643396a4a0fcd9cff82ad3c5863
许益样 <yiyang.xu@socialcredits.cn>
2020-05-13 20:39:10 +0800

    基本搭建完毕

---
+++ docs/.vuepress/components/my-btn.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="handleClick">我是按钮</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      console.log('click!')
+    }
+  }
+}
+</script>
+
